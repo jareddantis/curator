9047e90cad795845cf5543faf6d62ad21b2342bc
Jared Dantis <jareddantis@gmail.com>
2020-10-02 18:04:15 +0800

    Interceptors don't apply to SpotifyWebApi's internal Axios instance

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import interceptorSetup from "./api/interceptors";
-import { SpotifyWebApi } from "spotify-web-api-ts";
-
-interceptorSetup();
 
 createApp(App)
   .use(store)
   .use(router)
-  .provide("spotify", new SpotifyWebApi())
   .mount("#app");
